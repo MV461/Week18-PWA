2f0783ce7b379ab46ce7a8e0db8ea1148dcd1ab9
Milan Varughese <mv461@live.mdx.ac.uk>
2024-02-20 10:06:01 +0400

    service-worker.js: Tried to fix the 'fetch' event listener; wasn't working before.

--- service-worker.js
+++ service-worker.js
@@ -24,22 +24,16 @@ self.addEventListener('install', (e) => {
     );
 });
 
-self.addEventListener('fetch', (e) => {
+self.addEventListener('fetch', function (e) {
     e.respondWith(
-        // check if the cache has the file
-        caches.match(e.request)
-        .then((r) => {
-
-            console.log('[Service Worker] Fetching resource: ' + e.request.url);
-
-            // 'r' is the matching file if it exists in the cache
-            return r || fetch(e.request)
-            .then((response) => {
-                return caches.open(cacheName)
-                .then((cache) => {
-                    cache.put(e.request, response.clone);
+        caches.match(e.request).then(function (r) {
+            // Download the file if it is not in the cache,
+            return r || fetch(e.request).then(function (response) {
+                // add the new file to cache
+                return caches.open(cacheName).then(function (cache) {
+                    cache.put(e.request, response.clone());
                     return response;
-                })
+                });
             });
         })
     );
