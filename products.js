5df07de03df9cbfa3bab307ec24814dc6402bb61
Milan Varughese <mv461@live.mdx.ac.uk>
2024-02-20 08:59:52 +0400

    Initial Commit

---
+++ products.js
@@ -0,0 +1,47 @@
+let products = [
+    {
+        "id": 1001,
+        "title": "Cat Food, 25lb bag",
+        "description": "A 25 pound bag of <em>irresistable, organic goodness for your cat.</em>",
+        "price": 20.00,
+        "image": "images/catFood.jpg",
+        "availableInventory": 10,
+        "rating": 2
+    },
+    {
+        "id": 1002,
+        "title": "Yarn",
+        "description": "Yarn that your cat can play with for a <strong>very</strong> long time!",
+        "price": 8.00,
+        "image": "images/catYarn.jpg",
+        "availableInventory": 7,
+        "rating": 3
+    },
+    {
+        "id": 1003,
+        "title": "Kitty Litter",
+        "description": "Long-lasting and odorless.",
+        "price": 12.00,
+        "image": "images/catLitter.jpg",
+        "availableInventory": 5,
+        "rating": 4
+    },
+    {
+        "id": 1004,
+        "title": "Cat House",
+        "description": "A place for your cat to play!",
+        "price": 7.99,
+        "image": "images/catHouse.jpg",
+        "availableInventory": 11,
+        "rating": 5
+    },
+    {
+        "id": 1005,
+        "title": "Laser Pointer",
+        "description": "Drive your cat crazy with this <em>amazing</em> product.",
+        "price": 5.99,
+        "image": "images/catLaser.jpg",
+        "availableInventory": 10,
+        "rating": 3
+    }
+]
